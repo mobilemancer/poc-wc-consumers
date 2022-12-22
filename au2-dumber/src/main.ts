4ffaab4205a904d019f1e4a70c11843f372035a6
Andreas Wänqvist <andrkarl@gmail.com>
2022-12-23 00:09:43 +0100

    init to main wip: databind

--- au2-dumber/src/main.ts
+++ au2-dumber/src/main.ts
@@ -1,9 +1,10 @@
-import Aurelia from 'aurelia';
-import { RouterConfiguration } from '@aurelia/router';
-import { MyApp } from './my-app';
+import Aurelia from "aurelia";
+import { RouterConfiguration } from "@aurelia/router";
+import { MyApp } from "./my-app";
+import HeaderComponent from "poc-wc";
 
-Aurelia
-  .register(RouterConfiguration)
+Aurelia.register(RouterConfiguration)
+  .register(HeaderComponent)
   // To use HTML5 pushState routes, replace previous line with the following
   // customized router config.
   // .register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
--- au2-dumber/src/pages/web-components.ts
+++ au2-dumber/src/pages/web-components.ts
@@ -1,9 +1,19 @@
-import HeaderComponent from "poc-wc";
-
 export class WebComponents {
-    constructor() {
-        const x = new HeaderComponent();
+  private _greeting: string = "Hello!";
+  public get greeting(): string {
+    return this._greeting;
+  }
+  public set greeting(v: string) {
+    this._greeting = v;
+  }
 
-    }
+  constructor() {}
 
+  public button1Click() {
+    this._greeting = "Hello World!";
+  }
+
+  public button2Click() {
+    this._greeting = "Hello Space!";
+  }
 }
