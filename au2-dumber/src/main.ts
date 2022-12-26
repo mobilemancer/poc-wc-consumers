73b003bae915fa7303c7ccca118a98f0ab431cda
Andreas Wänqvist <andrkarl@gmail.com>
2022-12-26 11:55:24 +0100

    new import + clean

--- au2-dumber/src/main.ts
+++ au2-dumber/src/main.ts
@@ -1,24 +1,31 @@
-import { IContainer, IAttrMapper, AppTask, NodeObserverLocator, Aurelia } from 'aurelia';
+import {
+  IContainer,
+  IAttrMapper,
+  AppTask,
+  NodeObserverLocator,
+  Aurelia,
+} from "aurelia";
 import { RouterConfiguration } from "@aurelia/router";
 import { MyApp } from "./my-app";
-import HeaderComponent, { HeaderComponent2 } from "poc-wc";
+import { HeaderComponent, HeaderComponent2 } from "poc-wc";
 
 Aurelia.register(RouterConfiguration)
   .register(HeaderComponent)
   .register(HeaderComponent2)
   .register(
-    AppTask.creating(IContainer, container => {
+    AppTask.creating(IContainer, (container) => {
       const attrMapper = container.get(IAttrMapper);
       const nodeObserverLocator = container.get(NodeObserverLocator);
       attrMapper.useTwoWay((el, property) => {
         switch (el.tagName) {
-          case 'header-component2': return property === 'color';
+          case "header-component2":
+            return property === "color";
         }
       });
       nodeObserverLocator.useConfig({
-        'header-component2': {
-          value: { events: ['change'] }
-        }
+        "header-component2": {
+          value: { events: ["change"] },
+        },
       });
     })
   )
